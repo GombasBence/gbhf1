f8a6149ec4a25d19e762235016bbc488297f88ea
test <test@02-08-PC>
2017-02-09 11:56:37 +0100

    elmozdítottam a gombot

--- vg/Forrm1.Designer.cs
+++ vg/Forrm1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(126, 132);
+            this.button1.Location = new System.Drawing.Point(173, 197);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
